8c631448224052a16382f59ae810ba01d037d6e3
killianharr <killianh@live.ie>
2023-05-06 15:24:51 +0000

    final commit

--- main.cpp
+++ main.cpp
@@ -2,21 +2,19 @@
 
 int main()
 {
-    DigitalIn p13(p13);  // Initialize digital input on pin 13
-    DigitalIn p16(p16);  // Initialize digital input on pin 16
-    DigitalOut p23(p23); // Initialize digital output on pin 23
-    DigitalOut p25(p25); // Initialize digital output on pin 25
+    DigitalIn p13(p13);  
+    DigitalIn p16(p16); 
+    DigitalOut p23(p23); 
+    DigitalOut p25(p25); 
 
-    while(1) {
-        if(p13 == 1) {
+    while (1) {
+        if (p13 == 1) {   
             p23 = 1;
             p25 = 0;
-        }
-        else if(p16 == 1) {
+        } else if (p16 == 1) {
             p25 = 1;
             p23 = 0;
-        }
-        else {
+        } else {  
             p23 = 0;
             p25 = 0;
         }
